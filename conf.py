9bd96d8c08c6c6d035c092632e6d5753743e414d
Erich Ocean <erich.ocean@me.com>
2010-03-23 09:48:18 -0700

    changed default highlighting to JavaScript

--- conf.py
+++ conf.py
@@ -83,6 +83,8 @@ exclude_dirnames = ['.svn']
 # given in html_static_path.
 html_style = 'default.css'
 
+highlight_language = 'javascript'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
